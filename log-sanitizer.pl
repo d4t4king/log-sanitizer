69d76cf1f6be5ee0d72ef89ae5736ad3e9dcd022
dataking <dataking@gmail.com>
2016-08-26 01:14:46 -0700

    Fixed IP replacement for obfuscation.  Basic rewrite.  TBD mac mail hex

--- log-sanitizer.pl
+++ log-sanitizer.pl
@@ -7,28 +7,29 @@ use Term::ANSIColor;
 use Data::Dumper;
 use Getopt::Long qw/ :config no_ignore_case bundling /;
 
-my ($help, $verbose, $ip, $mac, $email, $hex, $input, $output);
+my ($help, $verbose, $cleanip, $cleanmac, $cleanmail, $cleanhex, $input, $output, $obfus);
 $verbose = 0;
 GetOptions(
 	'h|help'		=>	\$help,
 	'v|verbose+'	=>	\$verbose,
 	'i|input=s'		=>	\$input,
 	'o|output=s'	=>	\$output,
-	'ip:s'			=>	\$ip,
-	'mac:s'			=>	\$mac,
-	'email:s'		=>	\$email,
-	'x|hex'			=>	\$hex
+	'ip:s'			=>	\$cleanip,
+	'mac:s'			=>	\$cleanmac,
+	'email:s'		=>	\$cleanmail,
+	'x|hex'			=>	\$cleanhex,
+	'obfusips'		=>	\$obfus,
 );
 
 &usage() if ($help);
 
-$ip		= 'all' if ((defined($ip)) and ($ip eq ''));
-$mac 	= 'all' if ((defined($mac)) and ($mac eq ''));
-$email	= 'all' if ((defined($email)) and ($email eq ''));
+$cleanip		= 'all' if ((defined($cleanip)) and ($cleanip eq ''));
+$cleanmac 	= 'all' if ((defined($cleanmac)) and ($cleanmac eq ''));
+$cleanmail	= 'all' if ((defined($cleanmail)) and ($cleanmail eq ''));
 
-print colored("\$ip:\t $ip \n", "bold yellow")			if ((defined($ip)) and ($verbose));
-print colored("\$mac:\t $mac \n", "bold yellow")		if ((defined($mac)) and ($verbose));
-print colored("\$email:\t $email \n", "bold yellow")	if ((defined($email)) and ($verbose));
+print colored("\$cleanip:\t $cleanip \n", "bold yellow")			if ((defined($cleanip)) and ($verbose));
+print colored("\$mac:\t $cleanmac \n", "bold yellow")		if ((defined($cleanmac)) and ($verbose));
+print colored("\$email:\t $cleanmail \n", "bold yellow")	if ((defined($cleanmail)) and ($verbose));
 
 my $ip_rgx = qr/(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)/;
 my $mac_rgx = qr/(?:[0-9a-fA-F]{2}(:|-)){5}[0-9a-fA-F]{2}/;
@@ -36,26 +37,43 @@ my $mac_nc_rgx = qr/(?:[0-9a-fA-F]{2}(?:\:|-)){5}(?:\:|-)[0-9a-fA-F]{2}/;
 my $TLDs = qr/(?:com|net|org|edu|us|co.uk|biz|info)/;
 my $email_rgx = qr/[0-9a-zA-Z_.-]+\@(?:[0-9a-zA-Z-]+\.){1,3}$TLDs/;
 my $content = "";
-my %obfusips;
+my (%found_ips, %obfusips);
 
 open IN, "<$input" or die colored("There was a peoblem opening the input file ($input): $! \n", "bold red");
-while (<IN>) { $content .= $_; }
+while (my $line = <IN>) { 
+	while ($line =~ /($ip_rgx)/g) {
+		my $ip = $1;
+		$found_ips{$ip}++;
+		if ($obfus) { &obfusip($ip); }
+	}
+	$content .= $line; 
+}
 close IN or die colored("There was a problem closing the input file ($input): $! \n", "bold red");
 
-if (defined($ip)) {
-	if ($ip =~ /^all$/i) { 
-			$content =~ s/$ip_rgx/x.x.x.x/xsg; 
-	} elsif ($ip =~ /$ip_rgx/x) { $content =~ s/$ip/x.x.x.x/xsg; }
-} 
+print "Found ".scalar(keys(%found_ips))." total IPs. \n";
+print "Obfuscated ".scalar(keys(%obfusips))." IPs. \n";
+#print Dumper(\%obfusips);
 
-if (defined($mac)) {
-	if ($mac =~ /^all$/i) { $content =~ s/$mac_rgx/xx$1xx$1xx$1xx$1xx$1xx/xsg; } 
-	elsif ($mac =~ /$mac_rgx/x) { $content =~ s/$mac/xx$1xx$1xx$1xx$1xx$1xx/xsg; }
+if (defined($cleanip)) {
+	if ($cleanip =~ /^all$/i) {
+		foreach my $ip ( keys %found_ips ) {
+			if ($obfus) { $content =~ s/$ip/$obfusips{$ip}/xsg; } 
+			else { $content =~ s/$ip/d.e.a.d/xsg; }
+		}
+	} else {
+		if ($obfus) { $content =~ s/$cleanip/$obfusips{$cleanip}/xsg; }
+		else { $content =~ s/$cleanip/d.e.a.d/xsg; }
+	}
 }
 
-if (defined($email)) {
-	if ($email =~ /^all$/i) { $content =~ s/$email_rgx/xxx\@xxx.xxx/xsg; }
-	elsif ($email =~ /$email_rgx/x) { $content =~ s/$email/xxx\@xxx.xxx/xsg; }
+if (defined($cleanmac)) {
+	if ($cleanmac =~ /^all$/i) { $content =~ s/$mac_rgx/xx$1xx$1xx$1xx$1xx$1xx/xsg; } 
+	elsif ($cleanmac =~ /$mac_rgx/x) { $content =~ s/$cleanmac/xx$1xx$1xx$1xx$1xx$1xx/xsg; }
+}
+
+if (defined($cleanmail)) {
+	if ($cleanmail =~ /^all$/i) { $content =~ s/$email_rgx/xxx\@xxx.xxx/xsg; }
+	elsif ($cleanmail =~ /$email_rgx/x) { $content =~ s/$cleanmail/xxx\@xxx.xxx/xsg; }
 }
 
 open OUT, ">$output" or die colored("There was a problem opening the output file ($output) for writing: $! \n", "bold red");
@@ -95,5 +113,5 @@ sub obfusip {
 	my $r2 = int(rand(255));
 	print colored("R2: $r2 \n", "bold yellow") if ($verbose);
 	
-	$obfusip{$ip} = "10.$r1.$r2.$lo";
+	$obfusips{$ip} = "10.$r1.$r2.$lo";
 }
